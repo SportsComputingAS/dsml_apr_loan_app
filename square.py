0fad93af21af4877fc902cda302a84457919fe2f
Data Science with Harshit <61068902+dswh@users.noreply.github.com>
2023-03-03 22:39:26 +0530

    Add test cases

---
+++ square.py
@@ -0,0 +1,6 @@
+
+def get_square(a):
+    """
+    Returns the square of a number.
+    """
+    return a * a
---
+++ test_square.py
@@ -0,0 +1,7 @@
+from square import get_square
+
+
+def test_get_square():
+    a = 4
+    result = get_square(a)
+    assert result == 16
